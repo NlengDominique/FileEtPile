e26f928ea82fb5cb8321dc7f64bccb5239822981
NlengDominique <luqnleng5@gmail.com>
2023-12-15 11:10:46 -0800

    On branch main Changes to be committed: 	modified:   file.c

--- file.c
+++ file.c
@@ -6,13 +6,30 @@ File init_F(File F1){
 
 }
 int estVideFile(File F2){
+    if(F2.debut > F2.fin){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 
 }
 int estPleineFile(File F3){
-
+    if ((F3.debut < F3.fin) && (F3.fin = dmax - 1)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 File enfiler(File F4, int E){
-
+    if (estPleineFile(F4) == 1){
+        printf("File pleine.\n Impossible d'enfiler !");
+    }
+    else{
+        F4.fin++;
+        F4.tab[F4.fin] = E;
+    }
 }
 File defiler(File F5, int X){
 
