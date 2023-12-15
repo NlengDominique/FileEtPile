2d839df4899abda0fd7f5f166b5a33ad1572e4d1
NlengDominique <luqnleng5@gmail.com>
2023-12-15 10:28:28 -0800

    premier commit

---
+++ file.h
@@ -0,0 +1,30 @@
+#ifndef FILE_H_INCLUDED
+#define FILE_H_INCLUDED
+#define dmax 5
+typedef struct File File;
+
+struct File
+{
+   int tab [dmax] ;
+   int  debut;
+   int fin;
+};
+
+File init_F(File F1){
+
+}
+int estVideFile(File F2){
+
+}
+int estPleineFile(File F3){
+
+}
+File enfiler(File F4, int E){
+    
+}
+File defiler(File F5, int X){
+
+}
+
+
+#endif //FILE_H_INCLUDED
